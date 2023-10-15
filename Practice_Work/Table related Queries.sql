9f0fe38fa2fdb57b9cacda9980a323af8ccf49cb
Learning-stud <jayahalpara123456@gmail.com>
2023-10-16 04:31:10 +0530

    adding and changing the insert values into student ..

---
+++ Practice_Work/Table related Queries.sql
@@ -0,0 +1,18 @@
+create  database college;
+use college;
+
+create table student(
+Roll_no int primary key,
+Name varchar(50)
+);
+select * from student;
+
+
+insert into student(Roll_no,Name)
+values
+(101, "karan"),
+(102,"mahvere"),
+(103 , "ajay");
+
+
+insert into students values (104, "shyam");
