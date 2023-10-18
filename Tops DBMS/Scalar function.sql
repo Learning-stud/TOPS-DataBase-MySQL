66e91d742645350b08ab28dfec4359fd19b6ef0a
Learning-stud <jayahalpara123456@gmail.com>
2023-10-18 17:48:56 +0530

    scalar function...

---
+++ Tops DBMS/Scalar function.sql
@@ -0,0 +1,33 @@
+                                                        --------- Scaler Function--------------
+
+
+     set name ="devanshu"
+      where name=" jay "
+
+
+
+
+            --  to find the length  of name
+
+       select name,length(name) as length_of_name
+       from employee
+
+        -- to convert the name into lower case 
+
+           select name,lcase(name) as lower_case
+       from employee
+        -- to convert the name into upper case 
+
+           select name,ucase(name) as upper_case
+       from employee
+
+
+            --  to round up the point value
+
+          select round(salary)
+       from employee
+
+    -- to find the date time of the table but current periods
+       
+        select now()
+        from employee
