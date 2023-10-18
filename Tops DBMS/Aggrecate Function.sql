28fdee066b011c1eb670e9c1c377eb7662233a19
Learning-stud <jayahalpara123456@gmail.com>
2023-10-18 17:03:18 +0530

    .. aggrecate

---
+++ Tops DBMS/Aggrecate Function.sql
@@ -0,0 +1,21 @@
+use  finance
+
+--  to find the total sum of salary
+
+ select sum (salary)
+ from enployee;
+
+--  to find theaverage salary from emplayee
+
+
+ select avg (salary)
+ from emplayee;
+--    to find minimum salary from employee
+
+ select min (salary)
+ from emplayee;
+
+--  to find the maximum salry from employee
+
+ select max (salary)
+ from emplayee;
